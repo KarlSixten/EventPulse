881af86b729c89a09bc5fd43087510b0117c5616
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-05-04 15:31:04 +0200

    Added sessions and json middleware

--- server/app.js
+++ server/app.js
@@ -4,8 +4,22 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+import session from 'express-session';
+
+app.use(session({
+    secret: process.env.SESSIONSECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  }));
+
 import testRouter from './routers/testRouter.js';
 app.use(testRouter);
+
+import authRouter from './routers/authRouter.js';
+app.use(authRouter);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log("Server is running on port:", PORT));
