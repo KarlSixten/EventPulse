9c8bf7f89bf6fc098ca5078f92356aba5b8cafbc
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-05-04 15:28:36 +0200

    Added authRouter with sign up and login endpoint

---
+++ server/routers/authRouter.js
@@ -0,0 +1,78 @@
+import { Router } from "express";
+import db from '../database/connection.js'
+import { hashPassword, passwordMatchesHashed } from "../util/passwordHasher.js";
+
+const router = Router();
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+router.post("/api/sign-up", async (req, res) => {
+
+    if (!req.body || !req.body.email || !req.body.password || !req.body.firstName || !req.body.lastName) {
+        return res.status(400).send({ message: "Email, password, first and last name are required" });
+    }
+
+    const { email, password, firstName, lastName } = req.body;
+
+    if (!emailRegex.test(email)) {
+        return res.status(400).send({ message: "Invalid email format" });
+    }
+
+    try {
+        const result = await db.query('SELECT id FROM users WHERE email = $1', [email]);
+
+        if (result.rowCount > 0) {
+            return res.status(409).send({ message: `User with email ${email} already exists` });
+        } else {
+            const hashedPassword = await hashPassword(password);
+            await db.query('INSERT INTO users (email, password_hashed, first_name, last_name) VALUES ($1, $2, $3, $4)', [email, hashedPassword, firstName, lastName]);
+
+            return res.status(201).send({ message: "User created" });
+        }
+    } catch (error) {
+        console.error("Database error:", error);
+        return res.status(500).send({ message: 'Database error' });
+    }
+});
+
+router.post("/api/login", async (req, res) => {
+
+    if (!req.body || !req.body.email || !req.body.password) {
+        return res.status(400).send({ message: "Email and password are required" });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        const result = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+
+        if (result.rowCount === 0) {
+            return res.status(401).send({ message: "Invalid email or password" });
+        }
+
+        const user = result.rows[0];
+
+        if (!await passwordMatchesHashed(password, user.password_hashed)) {
+            return res.status(401).send({ message: "Invalid email or password" });
+        }
+
+        const userInfo = { 
+            id: user.id,
+            email: user.email,
+            firstName: user.first_name,
+            lastName: user.last_name
+        }
+
+        req.session.user = userInfo;
+
+        return res.status(200).send({
+            message: "Login successful",
+            user: userInfo
+        });
+    } catch (error) {
+        console.error("Login error:", error);
+        return res.status(500).send({ message: 'An internal error occurred during login' });
+    }
+});
+
+export default router;
