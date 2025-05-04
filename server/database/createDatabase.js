a1226b9762628d3782150709757e6b5d0478c24c
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-05-04 11:59:20 +0200

    Added logging when setting up db. Queries added to try/catch. Close db pool after setup

--- server/database/createDatabase.js
+++ server/database/createDatabase.js
@@ -2,12 +2,29 @@ import db from './connection.js';
 
 const deleteMode = process.argv.includes('--delete');
 
-if (deleteMode) {
-    await db.query(`DROP TABLE IF EXISTS users`);
-}
+console.log('Setting up database...');
+console.log(`Delete mode is ${deleteMode ? "enabled" : "disabled"}.`);
 
-await db.query(`CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
+
+try {
+    if (deleteMode) {
+        console.log('Dropping users table...');
+        await db.query(`DROP TABLE IF EXISTS users CASCADE`);
+        console.log('Users table dropped.');
+    }
+
+    console.log('Creating users table if it does not exist...');
+    await db.query(`CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL
 )`);
+    console.log('Users table created or already exists.');
+
+} catch (error) {
+    console.log("Error while setting up database:", error);
+} finally {
+    console.log('Closing database connection pool...');
+    await db.end();
+    console.log('Database connection pool closed.');
+}
