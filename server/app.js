e8a35d483e73b71d527b9418337b61d2d515f087
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-05-04 11:03:51 +0200

    Add pg, add testRouter

--- server/app.js
+++ server/app.js
@@ -2,5 +2,8 @@ import express from 'express';
 
 const app = express();
 
+import testRouter from './routers/testRouter.js';
+app.use(testRouter);
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log("Server is running on port:", PORT));
