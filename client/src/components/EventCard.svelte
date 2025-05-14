ef187e5321db61dd34fc18e8ca1a2e17476ce7f8
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-05-14 16:33:38 +0200

    EventCard now uses util functions for formatting

--- client/src/components/EventCard.svelte
+++ client/src/components/EventCard.svelte
@@ -1,25 +1,7 @@
 <script>
+    import { formatDate, formatDistance } from "../util/format";
+
     let { title, dateTime, distanceMeters} = $props()
-
-    function formatDistance(distance) {
-        if (distance < 1000) {
-            return distance.toFixed(0) + "m";
-        } else {
-            return (distance / 1000).toFixed(0) + "km";
-        }
-    }
-
-    function formatDate(isoString) {
-        const date = new Date(isoString);
-
-        const day = String(date.getUTCDate()).padStart(2, "0");
-        const month = String(date.getUTCMonth() + 1).padStart(2, "0");
-        const year = date.getUTCFullYear();
-        const hours = String(date.getUTCHours()).padStart(2, "0");
-        const minutes = String(date.getUTCMinutes()).padStart(2, "0");
-
-        return `${day}/${month}/${year} ${hours}:${minutes}`;
-    }
 </script>
 
 <main>
