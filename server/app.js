fa8847ee8e5565330f091e405f8a6a993cfdafe3
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-05-20 17:41:38 +0200

    Added socket.io + small refactor

--- server/app.js
+++ server/app.js
@@ -1,24 +1,52 @@
 import 'dotenv/config';
 
 import express from 'express';
-
 const app = express();
 
 app.use(express.json());
 
 import cors from 'cors';
-app.use(cors({
+const corsMiddleware = cors({
   origin: true, 
   credentials: true
-}));
+});
+app.use(corsMiddleware);
 
-import session from 'express-session';
-app.use(session({
-    secret: process.env.SESSIONSECRET,
+import session from 'express-session'
+const sessionMiddleware = session({
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }
-  }));
+});
+app.use(sessionMiddleware);
+
+import { createServer } from 'http';
+const httpServer = createServer(app);
+
+import { Server } from 'socket.io';
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true
+  }
+});
+
+io.engine.use(sessionMiddleware);
+
+io.on('connection', (socket) => {
+  const session = socket.request.session;
+
+  if (session && session.user && session.user.id) {
+    const userId = session.user.id;
+    const roomName = userId.toString();
+
+    socket.join(roomName);
+
+  } else {
+    socket.disconnect(true);
+  }
+});
 
 import authRouter from './routers/auth/authRouter.js';
 app.use(authRouter);
@@ -26,5 +54,7 @@ app.use(authRouter);
 import eventRouter from './routers/events/eventRouter.js';
 app.use(eventRouter);
 
+export { io };
+
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log("Server is running on port:", PORT));
+httpServer.listen(PORT, () => console.log("Server is running on port:", PORT));
