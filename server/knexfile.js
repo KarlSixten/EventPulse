7904ff0f36ceb0f08d6275f38e7eb2ef5f7eabb7
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-05-20 21:42:02 +0200

    Add knexfile.js for config

---
+++ server/knexfile.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+
+export default {
+    client: 'pg',
+    connection: {
+        host: process.env.PGHOST,
+        port: process.env.PGPORT,
+        user: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+    },
+    pool: {
+        min: 2,
+        max: 10
+    },
+};
