886747fa8532097530476b8c656a36e725b758ef
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-05-21 17:54:48 +0200

    Added placeholder edit event page

---
+++ client/src/pages/EditEvent/EditEvent.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+
+<h3>THIS IS EDIT EVENT</h3>
+
+
+<h1>TODO: PASS WHOLE EVENT DETAILS TO THIS PAGE TO PRE-FILL INPUTS</h1>
+
+
+<h3>THIS IS EDIT EVENT</h3>
+
+<style>
+
+</style>
