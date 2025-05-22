11417cce9d9b834912b4781587fcf87ec03167e4
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-05-22 15:41:15 +0200

    Added getLocalDateTimeString to util since it's used both in create and edit event

--- client/src/util/format.js
+++ client/src/util/format.js
@@ -17,3 +17,14 @@ export function formatDate(isoString) {
 
     return `${day}/${month}/${year} ${hours}:${minutes}`;
 }
+
+export function getLocalDateTimeString() {
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, "0");
+        const day = String(date.getDate()).padStart(2, "0");
+        const hours = String(date.getHours()).padStart(2, "0");
+        const minutes = String(date.getMinutes()).padStart(2, "0");
+
+        return `${year}-${month}-${day}T${hours}:${minutes}`;
+    }
