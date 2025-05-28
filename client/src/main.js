829afb0a91b218bd6e62a87254d4867a16e6940c
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-05-28 11:13:00 +0200

    Fixed bug where markers would not shown on map after npm run build

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,22 @@ import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+import iconUrl from 'leaflet/dist/images/marker-icon.png';
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+
+if (typeof window !== 'undefined') {
+    // @ts-ignore
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+        iconRetinaUrl: iconRetinaUrl,
+        iconUrl: iconUrl,
+        shadowUrl: shadowUrl,
+    });
+}
+
 const app = mount(App, {
   target: document.getElementById('app'),
 })
