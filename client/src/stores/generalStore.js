f76342c1bb629831d76cf25bf7a76199a9794e5f
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-06-14 23:22:27 +0200

    Add stripe publishable key to .env instead of store

--- client/src/stores/generalStore.js
+++ client/src/stores/generalStore.js
@@ -2,4 +2,4 @@ import { readable } from "svelte/store";
 
 export const BASE_URL = readable(import.meta.env.VITE_BASE_URL || "http://localhost:8080");
 
-export const STRIPE_PUBLISHABLE_KEY = readable("pk_test_51RYZ62Rs8LAMvSQRc1IwCKVaDEhqNIjx7krIqLAlG6ocBYAHI3sPoiuDk3HpGldLmHFYxrrWsyFHq9euf0f9mJFC007QlKiRcW")
+export const STRIPE_PUBLISHABLE_KEY = readable(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
