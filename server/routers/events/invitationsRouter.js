588d4d41963bd950a2b422681d5273b5c9e40749
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-06-14 19:51:27 +0200

    Re-add email sender which had accidently been removed

--- server/routers/events/invitationsRouter.js
+++ server/routers/events/invitationsRouter.js
@@ -98,6 +98,15 @@ router.post('/', isAuthenticated, async (req, res) => {
       };
 
       io.to(actualInviteeId.toString()).emit('new_notification', finalNotificationPayload);
+      // Commented out to lessen spam
+      /*
+      sendEventInvitationEmail(
+        normalizedInviteeEmail,
+        eventDetails,
+        messageContent,
+        inviterFirstName,
+      );
+      */
 
       return res.status(201).send({
         message: `Invitation sent successfully to ${inviteeEmailInput}.`,
