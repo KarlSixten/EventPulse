117b02dc810a8475ad5c38f73702e34adcd589cc
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-06-16 19:59:59 +0200

    Store sessions in db

--- server/middleware/sessionMiddleware.js
+++ server/middleware/sessionMiddleware.js
@@ -1,11 +1,21 @@
 import session from 'express-session';
+import { ConnectSessionKnexStore } from 'connect-session-knex';
+import knexPool from '../database/connection.js';
+
+const store = new ConnectSessionKnexStore({
+  knex: knexPool,
+  tablename: 'sessions',
+  createtable: true,
+});
 
 export default session({
+  store,
   secret: process.env.SESSIONSECRET,
   resave: false,
   saveUninitialized: false,
   cookie: {
     secure: false,
     httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
   },
 });
