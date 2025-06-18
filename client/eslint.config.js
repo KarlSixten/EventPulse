a71cdc609c9091fcd3a9657f6c3096c3aae2ecf8
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-06-18 12:42:01 +0200

    Add globals

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,7 +1,18 @@
 import js from '@eslint/js';
 import sveltePlugin from 'eslint-plugin-svelte';
+import globals from 'globals';
+
 
 export default [
   js.configs.recommended,
   ...sveltePlugin.configs['flat/recommended'],
+
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      }
+    }
+  }
 ];
