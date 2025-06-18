1876b554f6411470faed3435810ce2964d4bfc61
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-06-18 11:24:52 +0200

    Linter

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,12 +1,12 @@
 import 'dotenv/config';
 
 export default {
-    client: 'pg',
-    connection: {
-        host: process.env.PGHOST,
-        port: process.env.PGPORT,
-        user: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        database: process.env.PGDATABASE,
-    },
+  client: 'pg',
+  connection: {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+  },
 };
